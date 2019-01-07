b8955b2eb4e458bae76511e9481a2cc99d54d340
Thiago Rodrigues <thiago.rodrigues@inventsoftware.com.br>
2019-01-07 08:25:22 -0200

    Tratando tempo de fechamento

--- TesteDotNet/Program.cs
+++ TesteDotNet/Program.cs
@@ -27,7 +27,7 @@ namespace TesteDotNet
         private static void Despedida()
         {
             
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 1; i++)
             {
                 Console.Clear();
                 Console.WriteLine("Obrigado por utilizar nossa calculadora");
